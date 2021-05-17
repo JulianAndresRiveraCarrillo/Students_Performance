7ad6147e671e5d9f02d2e7701a97ac1e3f68f5f5
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2021-05-16 20:51:05 -0500

    base arbol  de decision

---
+++ Student_Performance/Model/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Performance.Model
+{
+    class dTree
+    {
+
+        private float nodeGainRatio;
+        private float nodeInformationGain;
+        private Boolean isLeaf;
+        private int majorityClass;
+        private int bestAttribute;
+        private dTree[] children;
+        private dTree parent;
+
+        public dTree(float nodeGainRatio, float nodeInformationGain, Boolean isLeaf, int majorityClass, int bestAttribute, dTree parent)
+        {
+
+            this.nodeGainRatio = nodeGainRatio;
+            this.nodeInformationGain = nodeInformationGain;
+            this.isLeaf = isLeaf;
+            this.majorityClass = majorityClass;
+            this.bestAttribute = bestAttribute;
+            this.parent = parent;
+            children = new 
+        }
+    }
+}
