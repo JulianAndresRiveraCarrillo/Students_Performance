dc0f26052ab7a9dd24efdb28ccb166d897010fd4
Julian A Rivera C <48961526+JulianAndresRiveraCarrillo@users.noreply.github.com>
2021-05-25 23:40:35 -0500

    Attribute class added in the new decision tree

---
+++ Student_Performance/Model/Attribute.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Student_Performance.Model
+{
+    class Attribute
+    {
+        public string Name { get; }
+
+        public List<string> DifferentAttributeNames { get; }
+
+        public double InformationGain { get; set; }
+
+        public Attribute(string name, List<string> differentAttributenames)
+        {
+            Name = name;
+            DifferentAttributeNames = differentAttributenames;
+        }
+
+        public static List<string> GetDifferentAttributeNamesOfColumn(DataTable data, int columnIndex)
+        {
+            var differentAttributes = new List<string>();
+
+            for (var i = 0; i < data.Rows.Count; i++)
+            {
+                var found = differentAttributes.Any(t => t.ToUpper().Equals(data.Rows[i][columnIndex].ToString().ToUpper()));
+
+                if (!found)
+                {
+                    differentAttributes.Add(data.Rows[i][columnIndex].ToString());
+                }
+            }
+
+            return differentAttributes;
+        }
+    }
+}
