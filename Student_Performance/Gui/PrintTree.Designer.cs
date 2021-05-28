6121ba1ecd1e663865e566afa06e17f45cd2741c
Julian A Rivera C <48961526+JulianAndresRiveraCarrillo@users.noreply.github.com>
2021-05-28 02:05:39 -0500

    little change in PrintTree.cs

--- Student_Performance/Gui/PrintTree.Designer.cs
+++ Student_Performance/Gui/PrintTree.Designer.cs
@@ -30,7 +30,6 @@ namespace Student_Performance.Gui
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PrintTree));
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -38,13 +37,6 @@ namespace Student_Performance.Gui
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // vScrollBar1
-            // 
-            this.vScrollBar1.Location = new System.Drawing.Point(659, 9);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(17, 172);
-            this.vScrollBar1.TabIndex = 0;
             // 
             // label1
             // 
@@ -57,26 +49,26 @@ namespace Student_Performance.Gui
             // 
             // panel1
             // 
+            this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.vScrollBar1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(685, 210);
+            this.panel1.Size = new System.Drawing.Size(638, 168);
             this.panel1.TabIndex = 2;
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.button1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 184);
+            this.panel2.Location = new System.Drawing.Point(0, 142);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(685, 26);
+            this.panel2.Size = new System.Drawing.Size(638, 26);
             this.panel2.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(157, 3);
+            this.button1.Location = new System.Drawing.Point(275, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -88,7 +80,7 @@ namespace Student_Performance.Gui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(685, 210);
+            this.ClientSize = new System.Drawing.Size(638, 168);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -103,8 +95,6 @@ namespace Student_Performance.Gui
         }
 
         #endregion
-
-        private System.Windows.Forms.VScrollBar vScrollBar1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
